07d434e5127b2dc90b2e433683070fe7e144eca9
Pranjal Mantri <pranjalmantri@gmail.com>
2026-07-23 16:42:54 +0530

    feat: create single run mode in agent with terminal UI

---
+++ agent/agent.py
@@ -0,0 +1,49 @@
+from typing import AsyncGenerator
+from agent.events import AgentEvent, AgentEventType
+from client.llm_client import LLMClient
+from client.response import StreamEventType, TokenUsage
+
+
+class Agent:
+    def __init__(self):
+        self.client = LLMClient()
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        if self.client:
+            await self.client.close()
+            self.client = None
+
+    async def run(self, message: str):
+        yield AgentEvent.agent_start(message)
+
+
+        final_response: str | None = None
+        usage: TokenUsage | None = None
+
+        async for event in self._agentic_loop():
+            yield event
+
+            if event.type == AgentEventType.TEXT_COMPLETE:
+                final_response = event.data.get("content")
+                usage = event.data.get("usage")
+
+        yield AgentEvent.agent_end(final_response, usage)
+
+    async def _agentic_loop(self) -> AsyncGenerator[AgentEvent, None]:
+        messages = [{"role": "user", "content": "How are you doing"}]
+        async for event in self.client.chat_completion(messages, True):
+
+            response_text = ""
+            if event.type == StreamEventType.TEXT_DELTA:
+                if event.text_delta:
+                    content = event.text_delta.content
+                    response_text += content
+                    yield AgentEvent.text_delta(content)
+            elif event.type == StreamEventType.ERROR:
+                yield AgentEvent.agent_error(event.error or "Unknown error occurred")
+
+            if response_text:
+                yield AgentEvent.text_complete(response_text)
--- main.py
+++ main.py
@@ -1,11 +1,47 @@
+from typing import Any
+
+from rich import get_console
+from agent.agent import Agent
 from client.llm_client import LLMClient
 import asyncio
+import click 
+from agent.events import AgentEventType
+from ui.renderer import UI
 
-async def main():
-    client = LLMClient()
+console = get_console()
 
-    messages = [{"role": "user", "content": "Hello"}]
-    async for event in client.chat_completion(messages, True):
-        print(event)
+class CLI:
+    def __init__(self):
+        self.agent: Agent | None = None
+        self.ui = UI(console)
+    
+    async def single_run(self, message: str):
+        async with Agent() as agent:
+            self.agent = agent
+            await self._process_message(message)
 
-asyncio.run(main())
+    async def _process_message(self, message: str):
+        if not self.agent:
+            return None 
+
+        async for event in self.agent.run(message):
+            if event.type == AgentEventType.TEXT_DELTA:
+                content = event.data.get("content", "")
+                self.ui.stream_assistant_delta(content)
+            elif event.type == AgentEventType.AGENT_ERROR:
+                self.ui.stream_assistant_delta(f"Something went wrong: {event.data["error"]}")
+            
+
+
+@click.command()
+@click.argument("prompt", required=False)
+def main(prompt: str):
+    cli = CLI()
+    # messages = [{"role": "user", "content": prompt}]
+
+    if prompt:
+        asyncio.run(cli.single_run(prompt))
+        
+
+main()
+
